587a6dbd668283714d0636474a158c0350dd2ef1
Nicolas Serbin <nicolas.serbin@gmail.com>
2023-12-22 13:27:49 -0300

    Feat: Updating terraform installer

--- install-terraform-and-terraform_docs.sh
+++ install-terraform-and-terraform_docs.sh
@@ -6,15 +6,57 @@ readonly TERRAFORM_DOCS_PATH="/usr/local/bin/terraform-docs"
 readonly TERRAFORM_PATH="/usr/local/bin/terraform"
 readonly TERRAGRUNT_PATH="/usr/local/bin/terragrunt"
 
-
-function is_tf_docs_installed() {
-  if [[ -f "${TERRAFORM_DOCS_PATH}" ]]; then
-    echo "=======================================" 
-    echo "Terraform-Docs is already installed at $(command -v terraform-docs)"
-    echo "Version: $(terraform-docs version)"
+function is_installed() {
+  local tool_path="$1"
+  local tool_name="$2"
+  
+  if [[ -f "$tool_path" ]]; then
+    echo "======================================="
+    echo "$tool_name is already installed at $(command -v "$tool_name")"
+    echo "Version: $($tool_name --version)"
     return 0
   else
     return 1
+  fi
+}
+
+function install_tool() {
+  local tool_name="$1"
+  local latest_version_url="$2"
+  
+  if is_installed "${TERRAFORM_DOCS_PATH}" "terraform-docs"; then
+    local current_version
+    current_version=$("${TERRAFORM_DOCS_PATH}" version | grep -oP 'v\d+\.\d+\.\d+')
+    local latest_version
+    latest_version=$(curl -sL "${latest_version_url}" | jq -r '.tag_name')
+    
+    if [[ "${current_version}" < "${latest_version}" ]]; then
+      echo "Current version ${current_version} is lower than the latest version ${latest_version}"
+      echo "Updating..."
+      sudo rm -f "${TERRAFORM_DOCS_PATH}"
+      check_system
+
+      case $OS_SYSTEM in
+        *"Linux"*) local tool_type="terraform-docs-${latest_version}-linux-amd64.tar.gz";;
+        *"Darwin"*) local tool_type="terraform-docs-${latest_version}-darwin-amd64.tar.gz";;
+        *)
+          echo "OS not supported"
+          exit 1
+          ;;
+      esac
+
+      local url="https://terraform-docs.io/dl/${latest_version}/${tool_type}"
+      wget "${url}"
+      tar -xzf "${tool_type}"
+      chmod +x "${tool_name}"
+      sudo mv "${tool_name}" "${TERRAFORM_DOCS_PATH}"
+      rm "${tool_type}"
+
+      echo "Installed: $(${tool_name} version)"
+    else
+      echo "${tool_name} is up to date."
+      return 0
+    fi
   fi
 }
 
@@ -23,146 +65,92 @@ function check_system(){
   OS_SYSTEM=$(uname -s)
 }
 
-function tf-docs-install() {
-  local LATEST_VERSION
-  LATEST_VERSION=$(curl -sL https://api.github.com/repos/terraform-docs/terraform-docs/releases/latest | jq -r '.tag_name')
-  if is_tf_docs_installed; then
-    local CURRENT_VERSION
-    CURRENT_VERSION=$(${TERRAFORM_DOCS_PATH} version | grep -oP 'v\d+\.\d+\.\d+')
-    if [[ "${CURRENT_VERSION}" < "${LATEST_VERSION}" ]]; then
-      echo "Current version ${CURRENT_VERSION} is lower than the latest version ${LATEST_VERSION}"
-      echo "Updating..."      
-      sudo rm -f "${TERRAFORM_DOCS_PATH}"
-      check_system
-
-        case $OS_SYSTEM in
-          *"Linux"*) TERRAFORM_DOCS_TYPE="terraform-docs-${LATEST_VERSION}-linux-amd64.tar.gz";;
-          *"Darwin"*) TERRAFORM_DOCS_TYPE="terraform-docs-${LATEST_VERSION}-darwin-amd64.tar.gz";;
-          *)
-            echo "OS not supported"
-            exit 1
-            ;;
-        esac
-
-        local URL
-        URL="https://terraform-docs.io/dl/${LATEST_VERSION}/${TERRAFORM_DOCS_TYPE}"
-        wget "${URL}"
-        tar -xzf "${TERRAFORM_DOCS_TYPE}"
-        chmod +x terraform-docs
-        sudo mv terraform-docs "${TERRAFORM_DOCS_PATH}"
-        rm "${TERRAFORM_DOCS_TYPE}"
-
-        echo "Installed: $(terraform-docs version)"      
-    else
-      echo "Terraform-Docs is up to date."
-      return 0
-    fi
-  fi
+function install_terraform() {
+  local tool_name="$1"
+  local latest_version_url="$2"
   
-}
-
-function is_tf_installed(){
-  if [[ -f "${TERRAFORM_PATH}" ]]; then
-    echo "======================================="
-    echo "Terraform already installed at ${TERRAFORM_PATH}"
-    echo "Version: $(terraform version)"
-    return 0
-  else
-    return 1
-  fi
-}
-
-function tf-install() {
-  if is_tf_installed; then
-    local CURRENT_VERSION
-    CURRENT_VERSION=$(terraform version | awk 'NR==1{print $2}')
-    local LATEST_VERSION
-    LATEST_VERSION=$(curl -sL https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1')
-    if [[ "${CURRENT_VERSION}" < "${LATEST_VERSION}" ]]; then
-      echo "Current version ${CURRENT_VERSION} is lower than the latest version ${LATEST_VERSION}"
-      echo "Updating..."      
+  if is_installed "${TERRAFORM_PATH}" "terraform"; then
+    local current_version
+    current_version=$("${TERRAFORM_PATH}" version | awk 'NR==1{print $2}')
+    local latest_version
+    latest_version=$(curl -sL "${latest_version_url}" | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1')
+    
+    if [[ "${current_version}" < "${latest_version}" ]]; then
+      echo "Current version ${current_version} is lower than the latest version ${latest_version}"
+      echo "Updating..."
       sudo rm -f "${TERRAFORM_PATH}"
       check_system
 
       case $OS_SYSTEM in
-        *"Linux"*) TERRAFORM_TYPE="terraform_${VERSION}_linux_amd64.zip";;
-        *"Darwin"*) TERRAFORM_TYPE="terraform_${VERSION}_darwin_amd64.zip";;
+        *"Linux"*) local tool_type="terraform_${latest_version}_linux_amd64.zip";;
+        *"Darwin"*) local tool_type="terraform_${latest_version}_darwin_amd64.zip";;
         *)
           echo "OS not supported"
           exit 1
           ;;
       esac
 
-      local URL="https://releases.hashicorp.com/terraform/${VERSION}/${TERRAFORM_TYPE}"
-      wget "${URL}"
-      unzip "${TERRAFORM_TYPE}"
-      sudo mv terraform "${TERRAFORM_PATH}"
-      rm "${TERRAFORM_TYPE}"
+      local url="https://releases.hashicorp.com/terraform/${latest_version}/${tool_type}"
+      wget "${url}"
+      unzip "${tool_type}"
+      sudo mv "${tool_name}" "${TERRAFORM_PATH}"
+      rm "${tool_type}"
+      
       if ! grep -qF "export PATH=${TERRAFORM_PATH}:\${PATH}" ~/.zshrc ; then
         echo "export PATH=${TERRAFORM_PATH}:\${PATH}" >> ~/.zshrc
       fi
       
-      echo "Installed: $(terraform version)"
+      echo "Installed: $(${tool_name} version)"
       echo "Run the following to reload your PATH with Terraform:"
       echo "source ~/.zshrc"
     else
-      echo "Terraform is up to date."
+      echo "${tool_name} is up to date."
       return 0
     fi
   fi  
+}
+
+function install_terragrunt() {
+  local tool_name="$1"
+  local latest_version_url="$2"
   
-}
-
-function is_tg_installed(){
-  if [[ -f "${TERRAGRUNT_PATH}" ]]; then
-    echo "======================================="
-    echo "Terragrunt already installed at ${TERRAGRUNT_PATH}"
-    echo "Version: $(terragrunt --version)"
-    return 0
-  else
-    return 1
-  fi
-}
-
-function tg-install(){
-  if is_tg_installed; then
-    local CURRENT_VERSION
-    CURRENT_VERSION=$(terragrunt --version | awk 'NR==1{print $2}')
-    local LATEST_VERSION
-    LATEST_VERSION=$(curl -sL https://api.github.com/repos/gruntwork-io/terragrunt/releases/latest | grep -o '"tag_name": *"[^"]*"' | cut -d'"' -f4)
-    if [[ "${CURRENT_VERSION}" < "${LATEST_VERSION}" ]]; then
-      echo "Current version ${CURRENT_VERSION} is lower than the latest version ${LATEST_VERSION}"
-      echo "Updating..."      
+  if is_installed "${TERRAGRUNT_PATH}" "terragrunt"; then
+    local current_version
+    current_version=$("${TERRAGRUNT_PATH}" --version | awk 'NR==1{print $2}')
+    local latest_version
+    latest_version=$(curl -sL "${latest_version_url}" | grep -o '"tag_name": *"[^"]*"' | cut -d'"' -f4)
+    
+    if [[ "${current_version}" < "${latest_version}" ]]; then
+      echo "Current version ${current_version} is lower than the latest version ${latest_version}"
+      echo "Updating..."
       sudo rm -f "${TERRAGRUNT_PATH}"
       check_system
 
       case $OS_SYSTEM in
-        *"Linux"*) TERRAGRUNT_TYPE="terragrunt_linux_amd64";;
-        *"Darwin"*) TERRAGRUNT_TYPE="terragrunt_darwin_amd64";;
+        *"Linux"*) local tool_type="terragrunt_linux_amd64";;
+        *"Darwin"*) local tool_type="terragrunt_darwin_amd64";;
         *)
           echo "OS not supported"
           exit 1
           ;;
       esac
-      local URL="https://github.com/gruntwork-io/terragrunt/releases/download/${VERSION}/${TERRAGRUNT_TYPE}"
-      wget "${URL}"
-      chmod +x "${TERRAGRUNT_TYPE}"
-      sudo mv "${TERRAGRUNT_TYPE}" "${TERRAGRUNT_PATH}"
-      rm "$TERRAGRUNT_TYPE"
 
-      echo "Installed: $(terragrunt --version)"
+      local url="https://github.com/gruntwork-io/terragrunt/releases/download/${latest_version}/${tool_type}"
+      wget "${url}"
+      chmod +x "${tool_type}"
+      sudo mv "${tool_type}" "${TERRAGRUNT_PATH}"
+      rm "${tool_type}"
+
+      echo "Installed: $(${tool_name} --version)"
     else
-      echo "Terragrunt is up to date."
+      echo "${tool_name} is up to date."
       return 0
     fi
   fi
 }
 
-tf-docs-install
+install_tool "terraform-docs" "https://api.github.com/repos/terraform-docs/terraform-docs/releases/latest"
 
-tf-install
+install_terraform "terraform" "https://api.github.com/repos/hashicorp/terraform/releases/latest"
 
-tg-install
-
-
-
+install_terragrunt "terragrunt" "https://api.github.com/repos/gruntwork-io/terragrunt/releases/latest"
